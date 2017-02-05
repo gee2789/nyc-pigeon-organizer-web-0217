ce87d87ed3c42480356f10c3d62061f5d66bad53
Gee Li <geeyung.li@gmail.com>
2017-02-05 15:37:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, h), result|
+    #each_with_object iterates over a collection and passes to the block.  useful for building up hashes.
+    #|key = names, h=attributes_hash|.  no block so creates as enumerator
+    #creates a hash of all levels down after names.
+    h.each do |value, names|
+      #iterates down to lower levels
+      #
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
 end
